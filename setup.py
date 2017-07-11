dddbccd3d27f4e46aa26a80b9d98bfc925f92ab5
Alex <eshobee@gmail.com>
2017-07-11 22:34:03 +0300

    bug fixes

--- setup.py
+++ setup.py
@@ -1,30 +1,37 @@
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup, find_packages
+    packages = find_packages(exclude=['tests'])
+except ImportError:
+    from distutils.core import setup
+    packages = ["pyYandexLinguistics"]
 
-
-if __name__ == "__main__":
-    setup(
-        name="pyYandexLinguistics",
-        version="0.1.0",
-        author="hell03end",
-        author_email="hell03end@outlook.com",
-        description="Yandex Linguistics APIs for Python 3.3+",
-        license="MIT License",
-        keywords="translate dictionary predict predictor yandex "
-                 "yandex-translate yandex-dictionary yandex-predictor",
-        url="https://github.com/hell03end/pyYandexLinguistics",
-        packages=find_packages(),
-        package_dir={'pyYandexLinguistics': 'pyYandexLinguistics'},
-        provides=['pyYandexLinguistics'],
-        classifiers=[
-            "Intended Audience :: Developers",
-            "Development Status :: 0.1 - Alpha",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 3.6"
-            "Programming Language :: Python :: 3.5"
-            "Programming Language :: Python :: 3.4"
-            "Programming Language :: Python :: 3.3"
-        ],
-        platforms=['All'],
-        install_requires=['requests>=2.9.1']
-    )
+setup(
+    name="pyYandexLinguistics",
+    packages=packages,
+    version="0.1.2rc1",
+    description="Yandex Linguistics APIs for Python 3.3+",
+    author="hell03end",
+    author_email="hell03end@outlook.com",
+    url="https://github.com/hell03end/pyYandexLinguistics",
+    keywords=[
+        "translate", "dictionary", "predict", "predictor", "yandex",
+        "linguistics", "yandex-translate", "yandex-dictionary",
+        "yandex-predictor"
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    license="MIT License",
+    platforms=["All"],
+    install_requires="requests>=2.9.1",
+    python_requires=">=3.3"
+)
