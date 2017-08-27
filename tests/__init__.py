85a4fb3517fe346fdece76fc24f8e06a934595e6
Alex <eshobee@gmail.com>
2017-08-27 12:07:44 +0300

    update tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,9 @@
 import unittest
 
 from .test_imports import TestImports
-from .test_main import TestTranslator, TestDictionary, TestPredictor
+from .test_main import (
+    TestTranslator, TestDictionary, TestPredictor, TestBaseTypes, TestSpeller
+)
 
 
 if __name__ == "__main__":
