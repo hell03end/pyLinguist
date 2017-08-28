d7dae47f3715a1273345187f240a253b9c1df776
Alex <eshobee@gmail.com>
2017-08-28 10:40:13 +0300

    add named params with hints

--- pyLinguist/__init__.py
+++ pyLinguist/__init__.py
@@ -1,33 +1,31 @@
 """
-    This module implements Yandex Translate and Dictionary APIs for Python 3.3+
+    This module implements some Yandex Linguistics APIs for Python 3.3+
 
-    Powered by Yandex.Translate — http://translate.yandex.com/
-    Powered by Yandex.Dictionary — https://tech.yandex.com/dictionary/
+    Powered by Yandex (https://tech.yandex.com)
 """
 
+from .baseTypes import YaTranslateException, _YaBaseAPIHandler
 from .config import logger
 
-from .baseTypes import YaTranslateException, _YaAPIHandler
 
-
-def Translator(*args, **kwargs):
+def Translator(api_key: str, xml: bool=False, version: str='1.5'):
     from .Translate import Translator
-    return Translator(*args, **kwargs)
+    return Translator(api_key=api_key, xml=xml, version=version)
 
 
-def Dictionary(*args, **kwargs):
+def Dictionary(api_key: str, xml: bool=False, version: str='1'):
     from .Vocabulary import Dictionary
-    return Dictionary(*args, **kwargs)
+    return Dictionary(api_key=api_key, xml=xml, version=version)
 
 
-def Predictor(*args, **kwargs):
+def Predictor(api_key: str, xml: bool=False, version: str='1'):
     from .Prediction import Predictor
-    return Predictor(*args, **kwargs)
+    return Predictor(api_key=api_key, xml=xml, version=version)
 
 
-def Speller(*args, **kwargs):
+def Speller(xml: bool=False, encoding: str='utf-8', **kwargs):
     from .Vocabulary import Speller
-    return Speller(*args, **kwargs)
+    return Speller(xml=xml, encoding=encoding, **kwargs)
 
 
 __all__ = ["Dictionary", "Translator", "YaTranslateException",
