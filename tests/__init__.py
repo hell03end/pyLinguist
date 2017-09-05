c9394024ad3983924c257bbd071fdb32f184e038
Alex <eshobee@gmail.com>
2017-09-05 12:32:17 +0300

    minor fixes

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,5 @@
 import unittest
 
-from .test_imports import TestImports
-from .test_main import (
-    TestTranslator, TestDictionary, TestPredictor, TestBaseTypes, TestSpeller
-)
-
 
 if __name__ == "__main__":
     unittest.main()
