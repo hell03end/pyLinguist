c18a5b8e83a7df97b06d0e7a7d34a5bfbf69bff5
Alex <eshobee@gmail.com>
2018-01-18 00:24:06 +0300

    move ERROR_CODES to exc

--- pyLinguist/exc.py
+++ pyLinguist/exc.py
@@ -1,4 +1,17 @@
-from pyLinguist.utils import ERROR_CODES
+import logging
+
+
+ERROR_CODES = {
+    400: "Wrong parameter was specified",
+    401: "Invalid API key",
+    402: "Blocked API key",
+    403: "Exceeded the daily limit on the amount of requests",
+    404: "Exceeded the daily limit on the amount of translated text",
+    413: "Exceeded the maximum text size",
+    422: "The text cannot be translated",
+    501: "The specified translation direction is not supported",
+    503: "Server not available",
+}
 
 
 class YaTranslateException(Exception):
@@ -7,7 +20,7 @@ class YaTranslateException(Exception):
 
     def __init__(self, status_code: int, *args, **kwargs):
         if not isinstance(status_code, int):
-            raise ValueError("int required, got {}".format(type(status_code)))
+            logging.warning("int required, got %s", type(status_code))
         message = self.error_codes.get(
             status_code, "Unknown code {}".format(status_code)
         )
